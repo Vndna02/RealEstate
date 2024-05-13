fd2836d02626c380cc4352bc329641edba31278b
Vndnaa <anandvandana2005@gmail.com>
2024-05-13 09:39:51 +0530

    create user update api route

--- api/index.js
+++ api/index.js
@@ -3,18 +3,23 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
     console.log('Connected to MongoDB!');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser())
 
 
 app.listen(3000, () => {
